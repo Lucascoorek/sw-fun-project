34debcfa1ec51388dba03453af4acf323f27c56b
Lukasz Kurek <lukascoorek@gmail.com>
2024-06-05 11:47:03 +0200

    addjust card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { SVGComponent } from '../svg/svg.component';
 import { SVGModel } from '../svg/svg-model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MaterialModule, SVGComponent],
+  imports: [MaterialModule, SVGComponent, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() name: SVGModel = 'trooper';
+  @Input() name?: SVGModel;
+  @Input() title?: string = '';
 }
